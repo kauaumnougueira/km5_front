288710fac36b0b46aa8168bd53b46c7c8f9aa10f
kauaumonougueira <kaua_kna@hotmail.com>
2023-04-21 01:49:24 -0300

    header

---
+++ js/spa.js
@@ -0,0 +1,9 @@
+function render(div, component){
+    //fetch de render
+    fetch(component)
+        .then(response => response.text())
+        .then(html => {
+            div.innerHTML = html
+        })
+        .catch(error => console.error(error));
+}
